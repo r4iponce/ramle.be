18ce686dbc59d39795677a6342df59f2b3788546
Ramle <no@users.noreply>
2022-02-15 15:44:35 +0100

    Add translation

---
+++ static/js/translate.js
@@ -0,0 +1,28 @@
+var tran = new Translater();
+
+var userLang = navigator.language || navigator.userLanguage;
+
+if (userLang.includes("fr")) {
+  tran.setLang('fr');
+} else if (userLang == "nl") {
+  tran.setLang('nl');
+} else {
+  tran.setLang("default");
+}
+
+function updateLang(lang) {
+  tran.setLang(lang);
+  toggleTranslationPanel()
+}
+
+toggled = false;
+
+function toggleTranslationPanel() {
+  if (toggled) {
+    document.getElementById('translationContainer').style.display = 'none'
+    toggled = !toggled
+  } else {
+    document.getElementById('translationContainer').style.display = 'block'
+    toggled = !toggled;
+  }
+}
